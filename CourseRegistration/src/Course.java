a462aad254065dc2d0cb13c252a575dd670317aa
xLightning06x <nawazomair545@gmail.com>
2026-05-02 16:52:56 +0300

    Fixed Student, Course classes and added Serializable

--- CourseRegistration/src/Course.java
+++ CourseRegistration/src/Course.java
@@ -1,50 +1,33 @@
+import java.io.Serializable;
+import Queue.LinkedQueue;
 import Queue.Queue;
 import Tree.Tree;
 
-public class Course {
+public class Course implements Serializable {
     private int CRN;
     private String name;
     private int capacity;
     private Tree<Student> enrolled;
     private Queue<Student> waitingList;
 
-    public int getCRN() {
-        return CRN;
-    }
-
-    public void setCRN(int CRN) {
+    public Course(int CRN, String name, int capacity) {
         this.CRN = CRN;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
         this.capacity = capacity;
+        this.enrolled = new Tree<>();        // empty BST for enrolled students
+        this.waitingList = new LinkedQueue<>(); // empty queue for waiting list
     }
 
-    public Tree<Student> getEnrolled() {
-        return enrolled;
-    }
+    public int getCRN() { return CRN; }
+    public void setCRN(int CRN) { this.CRN = CRN; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getCapacity() { return capacity; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
+    public Tree<Student> getEnrolled() { return enrolled; }
+    public Queue<Student> getWaitingList() { return waitingList; }
 
-    public void setEnrolled(Tree<Student> enrolled) {
-        this.enrolled = enrolled;
-    }
-
-    public Queue<Student> getWaitingList() {
-        return waitingList;
-    }
-
-    public void setWaitingList(Queue<Student> waitingList) {
-        this.waitingList = waitingList;
+    public String toString() {
+        return "CRN: " + CRN + " | Course: " + name + " | Capacity: " + capacity;
     }
 }
